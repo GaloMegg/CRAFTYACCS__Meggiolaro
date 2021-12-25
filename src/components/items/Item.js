81708dbe3e347e9a4f30b166de3811c9ad95fe8e
WtsCrackin <galomeggiolarobul@gmail.com>
2021-12-25 19:06:02 -0300

    item changes

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,13 +1,13 @@
 import ItemCount from "./ItemCount"
 const Item = ({ stock, productName, description, price }) => {
-    const OnAdd = ()=>{}
+    const OnAdd = () => { }
     return (
         <div className="product__card">
             <img src="" alt="" className="product__card--img" />
             <p>{productName}</p>
             <p>{description}</p>
             <p>{price}</p>
-            <ItemCount stock={stock} initial={1} onAdd={OnAdd}/>
+            <ItemCount stock={stock} initial={1} onAdd={OnAdd} />
         </div>
     )
 }
