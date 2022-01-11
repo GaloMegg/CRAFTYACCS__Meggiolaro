9859d04cce2b2f5b741a7cf85a17903b8fd6e44b
WtsCrackin <galomeggiolarobul@gmail.com>
2022-01-11 04:48:10 -0300

    whowe are content, starting to style

--- src/components/bodyapp/whoweare/Whoweare.js
+++ src/components/bodyapp/whoweare/Whoweare.js
@@ -1,8 +1,17 @@
+import whoWeAreImage from "./images/craftyaccslogo.png"
 const Whoweare = () => {
     return (
-        <div>
-            <p>quienesomos papa</p>
-        </div>
+
+        <section className="whoweare">
+            <div className="whoweare__flex-a">
+                <img src={whoWeAreImage} alt="" className="whoweare__flex-a--try" />
+            </div>
+            <div className="whoweare__flex-b">
+                <h3 className="whoweare__flex-b--title">Crafty Accesorios</h3>
+                <p className="whoweare__flex-b--slogan"> <i> Lo lindo de crear...</i></p>
+                <p className="whoweare__flex-b--description">Somos un grupo de personas dedicadas a la artesania con el objetivo de formar parte de la vida de los clientes, creamos cualquier tipo de proyecto, pero amamos la joyeria de autor y los accesorios personales. Recidimos en la capital de argentina, pero somos del mundo. Internacionales y locos.</p>
+            </div>
+        </section>
     )
 }
 
