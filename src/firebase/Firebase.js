0f2948138c09c1f973bd752d73fa36526001acfb
WtsCrackin <galomeggiolarobul@gmail.com>
2022-01-27 09:19:46 -0300

    .env

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYGE1JlffLqNQWiHB_yk60gZOroU1L7Q4",
-    authDomain: "crafty-accesorios.firebaseapp.com",
-    projectId: "crafty-accesorios",
-    storageBucket: "crafty-accesorios.appspot.com",
-    messagingSenderId: "662755013196",
-    appId: "1:662755013196:web:c22b2b46ebe6c29d7e7ae9",
-    measurementId: "G-W5YLC9RBPQ"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
